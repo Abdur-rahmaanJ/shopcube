25dd98f4cc42d56398fbd7ace1baa0c30f4a5f4e
ouyangjunfei <1342597034@qq.com>
2019-07-12 11:49:59 +0800

    change Date

--- shopyo/application/models.py
+++ shopyo/application/models.py
@@ -7,7 +7,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
 class Products(db.Model):
     __tablename__ = 'products'
     barcode = db.Column(db.String(100), primary_key=True)
@@ -15,7 +14,6 @@ class Products(db.Model):
     vat_price = db.Column(db.Float)
     selling_price = db.Column(db.Float)
     manufacturer = db.Column(db.String(100), db.ForeignKey('manufacturers.name'))
-
 
 class People(db.Model):
     __tablename__ = 'people'
@@ -25,25 +23,21 @@ class People(db.Model):
     about = db.Column(db.String(100))
     social_media = db.Column(db.String(100))
 
-
 class Manufacturers(db.Model):
     __tablename__ = 'manufacturers'
     name = db.Column(db.String(100), primary_key=True)
-
 
 class Appointments(db.Model):
     __tablename__ = 'appointments'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     date = db.Column(db.String(20))
-    active = db.Column(db.String(1))  # active应该使用boolean类型,这里还有点问题
-
+    active = db.Column(db.String(1))
 
 class Settings(db.Model):
     __tablename__ = 'settings'
     setting = db.Column(db.String(100), primary_key=True)
     value = db.Column(db.String(100))
-
 
 class Patients(db.Model):
     __tablename__ = 'patients'
