190110b94acb021b0e37130e1a25807ea7575df6
blips5 <33946380+blips5@users.noreply.github.com>
2019-07-22 22:53:46 +0100

    Added a time input for appointments

--- shopyo/application/models.py
+++ shopyo/application/models.py
@@ -32,6 +32,7 @@ class Appointments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     date = db.Column(db.String(20))
+    time = db.Column(db.String(20))
     active = db.Column(db.String(20))
 
 class Settings(db.Model):
