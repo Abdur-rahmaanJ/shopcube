cd4657b7f9ecbac81fbb0b64bf2ea2f006730af7
Abdur-RahmaanJ <arj.python@gmail.com>
2019-07-29 07:42:58 +0400

    removed manage.py

--- shopyo/modules/patient/view.py
+++
@@ -1,8 +0,0 @@
-from flask import (
-    Blueprint, render_template, request, redirect, url_for
-    )
-from models import db, Patients
-from flask_sqlalchemy import sqlalchemy
-from settings import get_value
-
-patient_blueprint = Blueprint('patient', __name__, url_prefix='/patient')
