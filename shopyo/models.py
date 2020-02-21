620e4d00867029dfd99ae78235ad0dbb77b0726b
theimperium20 <survesh.in.heaven@gmail.com>
2020-02-21 17:24:51 +0530

    Added more information to Products

--- shopyo/models.py
+++ shopyo/models.py
@@ -25,7 +25,14 @@ class Products(db.Model):
     __tablename__ = 'products'
     barcode = db.Column(db.String(100), primary_key=True)
     price = db.Column(db.Float)#
-
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(300))
+    category = db.Column(db.String(50))
+    category_id = db.Column(db.Integer)
+    stock = db.Column(db.Integer)
+    supplier = db.Column(db.String(100))
+    supplier_id = db.Column(db.Integer)
+    discontinued = db.Column(db.Boolean)
     vat_price = db.Column(db.Float)
     selling_price = db.Column(db.Float)
     manufacturer = (db.Column(db.String(100),
