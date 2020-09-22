9d16e079d3ffadad9fbb260f30950962581b08b8
Abdur-RahmaanJ <arj.python@gmail.com>
2020-09-22 20:10:56 +0400

    bug fix

--- shopyo/__main__.py
+++ shopyo/__main__.py
@@ -4,7 +4,7 @@ from . import shopyoapi
 
 def main(args):
     if args[1] == "new" and len(args) == 4:
-        shopyoapi.new_project(args[2], args[3])
+        shopyoapi.cmd.new_project(args[2], args[3])
 
 
 if __name__ == "__main__":
