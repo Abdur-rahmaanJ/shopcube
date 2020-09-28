2b1e4e69cee3a0100ee5a047981fbaf132d8ad8b
Abdur-RahmaanJ <arj.python@gmail.com>
2020-09-28 10:30:53 +0400

    added forms.py to auto flash form errors

---
+++ shopyo/shopyoapi/forms.py
@@ -0,0 +1,12 @@
+from userapi.html import notify_warning
+from flask import flash
+
+def flash_errors(form):
+    """Flashes form errors"""
+    for field, errors in form.errors.items():
+        for error in errors:
+            error_msg = u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            )
+            flash(notify_warning(error_msg))
