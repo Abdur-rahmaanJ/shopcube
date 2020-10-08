34c3e1730fdd4ba4af7377bd80e3b56a8b01273d
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-10-08 01:44:18 -0700

    refactor: checking module name to follow certain constraints

--- shopyo/shopyoapi/cmd.py
+++ shopyo/shopyoapi/cmd.py
@@ -3,11 +3,10 @@ import shutil
 import sys
 import subprocess
 import json
-
+import re
 
 from shopyoapi.uploads import add_admin
 from shopyoapi.uploads import add_setting
-
 
 
 def clean():
@@ -60,6 +59,9 @@ def initialise():
 
 
 def create_module(modulename):
+    if bool(re.match(r'^[A-Za-z0-9_]+$', modulename)) is False:
+        print("Error: modulename is not valid, please use alphanumeric and underscore only")
+        sys.exit()
     print('creating module: {}'.format(modulename))
     base_path = 'modules/' + modulename
     trymkdir(base_path)
