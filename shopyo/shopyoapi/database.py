c653c237940c77ebde99f4175c4f03a0fe5e7ca9
Abdur-RahmaanJ <arj.python@gmail.com>
2021-01-06 12:57:28 +0400

    autoload caters for boxes, closes #251

--- shopyo/shopyoapi/database.py
+++ shopyo/shopyoapi/database.py
@@ -12,12 +12,27 @@ def autoload_models():
     None
     """
     print("Auto importing models")
-    for module in os.listdir("modules"):
-        if module.startswith("__"):
+    for folder in os.listdir("modules"):
+        if folder.startswith("__"):
             continue
-        try:
-            to_load = "modules.{}.models".format(module)
-            importlib.import_module(to_load)
-            print("[x]", "imported", to_load)
-        except Exception as e:
-            print("[ ]", e)
+        elif folder.startswith('box__'):
+            for sub_folder in os.listdir(
+                os.path.join("modules", folder)
+            ):
+                if sub_folder.startswith("__"):  # ignore __pycache__
+                    continue
+                elif sub_folder.endswith(".json"):  # box_info.json
+                    continue
+                try:
+                    to_load_submodel = "modules.{}.{}.models".format(folder, sub_folder)
+                    importlib.import_module(to_load_submodel)
+                    print("[x]", "imported", to_load_submodel)
+                except Exception as e:
+                    print("[ ]", e)
+        else:
+            try:
+                to_load = "modules.{}.models".format(folder)
+                importlib.import_module(to_load)
+                print("[x]", "imported", to_load)
+            except Exception as e:
+                print("[ ]", e)
