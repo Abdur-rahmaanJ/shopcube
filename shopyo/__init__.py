0947773caa223162f5c4907152caf2b13a6ce74c
Abdur-RahmaanJ <arj.python@gmail.com>
2021-01-09 09:42:41 +0400

    fix: please login for access using shopyo notifications

--- shopyo/__init__.py
+++ shopyo/__init__.py
@@ -1,2 +1,2 @@
-version_info = (3, 3, 4)
+version_info = (3, 3, 5)
 __version__ = ".".join([str(v) for v in version_info])
--- shopyo/modules/box__default/admin/admin.py
+++ shopyo/modules/box__default/admin/admin.py
@@ -7,11 +7,12 @@ from flask import url_for
 from flask_login import current_user
 
 from shopyoapi.init import login_manager
+from shopyoapi.html import notify_warning
 
 from modules.box__default.admin.models import User
 
 login_manager.login_view = "login.login"
-login_manager.login_message = "Please login for access"
+login_manager.login_message = notify_warning("Please login for access")
 
 
 @login_manager.user_loader
