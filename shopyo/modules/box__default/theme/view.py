c8e690bf9eb3796b5ce530686e92bc17c3c1ea43
Abdur-RahmaanJ <arj.python@gmail.com>
2021-01-27 23:14:47 +0400

    fix: added panel-control for back theme

--- shopyo/modules/box__default/theme/view.py
+++ shopyo/modules/box__default/theme/view.py
@@ -56,10 +56,25 @@ def index():
         with open(info_path) as f:
             all_front_info[folder] = json.load(f)
 
-    active_front_theme = get_setting("ACTIVE_FRONT_THEME")
+    back_themes_path = os.path.join(current_app.config["BASE_DIR"], "themes", "back")
+    all_back_info = {}
+    back_theme_folders = get_folders(back_themes_path)
+    for folder in back_theme_folders:
+        theme_path = os.path.join(back_themes_path, folder)
+        info_path = os.path.join(theme_path, "info.json")
+        with open(info_path) as f:
+            all_back_info[folder] = json.load(f)
 
-    
-    context.update({"all_front_info": all_front_info, "active_front_theme": active_front_theme})
+    active_front_theme = get_setting("ACTIVE_FRONT_THEME")
+    active_back_theme = get_setting("ACTIVE_BACK_THEME")
+
+
+    context.update({
+        "all_front_info": all_front_info, 
+        "all_back_info": all_back_info,
+        "active_front_theme": active_front_theme,
+        "active_back_theme": active_back_theme
+        })
     context.update(module_settings)
 
     return render_template(
@@ -71,6 +86,17 @@ def index():
 @login_required
 def activate_front_theme(theme_name):
     set_setting("ACTIVE_FRONT_THEME", theme_name)
+
+    # with app.app_context():
+
+    # current_app.jinja_loader,
+    # print(current_app.jinja_loader.list_templates())
+    return redirect(url_for("{}.index".format(module_info["module_name"])))
+
+@module_blueprint.route("/activate/back/<theme_name>")
+@login_required
+def activate_back_theme(theme_name):
+    set_setting("ACTIVE_BACK_THEME", theme_name)
 
     # with app.app_context():
 
