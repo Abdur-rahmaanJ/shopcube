86dc685a6fda79d91985f94ea3ba8d041d4ef688
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-11-16 00:32:25 +0400

    chore: update resource

--- src/shopcube/modules/resource/view.py
+++ src/shopcube/modules/resource/view.py
@@ -11,7 +11,7 @@ from flask_login import login_required
 from PIL import Image as PILimage
 
 # from modules.box__ecommerce.product.models import Product
-from modules.resource.models import Image
+# from modules.resource.models import Image
 
 # from flask import url_for
 
@@ -50,101 +50,101 @@ def index():
     return module_info["display_string"]
 
 
-@module_blueprint.route("/theme/front/<active_theme>/styles.css", methods=["GET"])
-def active_front_theme_css(active_theme):
-    theme_dir = os.path.join(
-        current_app.config["BASE_DIR"],
-        "static",
-        "themes",
-        "front",
-        active_theme,
-    )
-    # return theme_dir
-    return send_from_directory(theme_dir, "styles.css")
+# @module_blueprint.route("/theme/front/<active_theme>/styles.css", methods=["GET"])
+# def active_front_theme_css(active_theme):
+#     theme_dir = os.path.join(
+#         current_app.config["BASE_DIR"],
+#         "static",
+#         "themes",
+#         "front",
+#         active_theme,
+#     )
+#     # return theme_dir
+#     return send_from_directory(theme_dir, "styles.css")
 
 
-@module_blueprint.route("/theme/back/<active_theme>/styles.css", methods=["GET"])
-def active_back_theme_css(active_theme):
-    theme_dir = os.path.join(
-        current_app.config["BASE_DIR"],
-        "static",
-        "themes",
-        "back",
-        active_theme,
-    )
-    # return theme_dir
-    return send_from_directory(theme_dir, "styles.css")
+# @module_blueprint.route("/theme/back/<active_theme>/styles.css", methods=["GET"])
+# def active_back_theme_css(active_theme):
+#     theme_dir = os.path.join(
+#         current_app.config["BASE_DIR"],
+#         "static",
+#         "themes",
+#         "back",
+#         active_theme,
+#     )
+#     # return theme_dir
+#     return send_from_directory(theme_dir, "styles.css")
 
 
-@module_blueprint.route("/product/<filename>", methods=["GET"])
-def product_image(filename):
+# @module_blueprint.route("/product/<filename>", methods=["GET"])
+# def product_image(filename):
 
-    # return theme_dir
-    if filename == "default":
-        return send_from_directory(
-            os.path.join(current_app.config["BASE_DIR"], "static", "default"),
-            "default_product.jpg",
-        )
-    return send_from_directory(
-        current_app.config["UPLOADED_PRODUCTPHOTOS_DEST"], filename
-    )
+#     # return theme_dir
+#     if filename == "default":
+#         return send_from_directory(
+#             os.path.join(current_app.config["BASE_DIR"], "static", "default"),
+#             "default_product.jpg",
+#         )
+#     return send_from_directory(
+#         current_app.config["UPLOADED_PRODUCTPHOTOS_DEST"], filename
+#     )
 
 
-# Handles javascript image uploads from tinyMCE
-@module_blueprint.route("/upload/tinymce/image", methods=["POST"])
-@login_required
-def upload_tinymce_image():
-    # Kevin Foong
-    file = request.files.get("file")
-    if file:
-        filename = file.filename.lower()
-        fn, ext = filename.split(".")
-        # truncate filename (excluding extension) to 30 characters
-        fn = fn[:30]
-        filename = fn + "." + ext
-        if ext in ["jpg", "gif", "png", "jpeg"]:
-            try:
-                # everything looks good, save file
-                img_fullpath = os.path.join(
-                    current_app.config["UPLOADED_PATH_IMAGE"], filename
-                )
-                file.save(img_fullpath)
-                # get the file size to save to db
-                file_size = os.stat(img_fullpath).st_size
-                size = 160, 160
-                # read image into pillow
-                im = PILimage.open(img_fullpath)
-                # get image dimension to save to db
-                file_width, file_height = im.size
-                # convert to thumbnail
-                im.thumbnail(size)
-                thumbnail = fn + "-thumb.jpg"
-                tmb_fullpath = os.path.join(
-                    current_app.config["UPLOADED_PATH_THUMB"], thumbnail
-                )
-                # PNG is index while JPG needs RGB
-                if not im.mode == "RGB":
-                    im = im.convert("RGB")
-                # save thumbnail
-                im.save(tmb_fullpath, "JPEG")
+# # Handles javascript image uploads from tinyMCE
+# @module_blueprint.route("/upload/tinymce/image", methods=["POST"])
+# @login_required
+# def upload_tinymce_image():
+#     # Kevin Foong
+#     file = request.files.get("file")
+#     if file:
+#         filename = file.filename.lower()
+#         fn, ext = filename.split(".")
+#         # truncate filename (excluding extension) to 30 characters
+#         fn = fn[:30]
+#         filename = fn + "." + ext
+#         if ext in ["jpg", "gif", "png", "jpeg"]:
+#             try:
+#                 # everything looks good, save file
+#                 img_fullpath = os.path.join(
+#                     current_app.config["UPLOADED_PATH_IMAGE"], filename
+#                 )
+#                 file.save(img_fullpath)
+#                 # get the file size to save to db
+#                 file_size = os.stat(img_fullpath).st_size
+#                 size = 160, 160
+#                 # read image into pillow
+#                 im = PILimage.open(img_fullpath)
+#                 # get image dimension to save to db
+#                 file_width, file_height = im.size
+#                 # convert to thumbnail
+#                 im.thumbnail(size)
+#                 thumbnail = fn + "-thumb.jpg"
+#                 tmb_fullpath = os.path.join(
+#                     current_app.config["UPLOADED_PATH_THUMB"], thumbnail
+#                 )
+#                 # PNG is index while JPG needs RGB
+#                 if not im.mode == "RGB":
+#                     im = im.convert("RGB")
+#                 # save thumbnail
+#                 im.save(tmb_fullpath, "JPEG")
 
-                # save to db
-                img = Image(
-                    filename=filename,
-                    thumbnail=thumbnail,
-                    file_size=file_size,
-                    file_width=file_width,
-                    file_height=file_height,
-                )
-                db.session.add(img)
-                db.session.commit()
-            except OSError:
-                output = make_response(404)
-                output.headers["Error"] = "Cannot create thumbnail for " + filename
-                return output
-            return jsonify({"location": filename})
+#                 # save to db
+#                 img = Image(
+#                     filename=filename,
+#                     thumbnail=thumbnail,
+#                     file_size=file_size,
+#                     file_width=file_width,
+#                     file_height=file_height,
+#                 )
+#                 db.session.add(img)
+#                 db.session.commit()
+#             except OSError:
+#                 output = make_response(404)
+#                 output.headers["Error"] = "Cannot create thumbnail for " + filename
+#                 return output
+#             return jsonify({"location": filename})
 
-    # fail, image did not upload
-    output = make_response(404)
-    output.headers["Error"] = "Filename needs to be JPG, JPEG, GIF or PNG"
-    return output
+#     # fail, image did not upload
+#     output = make_response(404)
+#     output.headers["Error"] = "Filename needs to be JPG, JPEG, GIF or PNG"
+#     return output
