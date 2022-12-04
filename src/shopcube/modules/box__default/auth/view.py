a65f8cfd14f2876d52de004e8042782e31998635
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-12-04 10:37:28 +0400

    tests: workflow

--- src/shopcube/modules/box__default/auth/view.py
+++ src/shopcube/modules/box__default/auth/view.py
@@ -2,21 +2,23 @@ import json
 import os
 
 from flask import Blueprint
+from flask import current_app
 from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
+from flask import session
 from flask import url_for
-
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
-from shopyo.api.html import notify_danger
-from shopyo.api.html import notify_success
-
+from flask_login import user_logged_out
+from flask_login.utils import _get_user
 from modules.box__default.admin.models import User
 from modules.box__default.auth.forms import LoginForm
 from modules.box__default.auth.forms import RegistrationForm
+from shopyo.api.html import notify_danger
+from shopyo.api.html import notify_success
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
 module_info = {}
@@ -102,6 +104,27 @@ def shop_login():
 @login_required
 def logout():
     logout_user()
+    user = _get_user()
+
+    if "_user_id" in session:
+        session.pop("_user_id")
+
+    if "_fresh" in session:
+        session.pop("_fresh")
+
+    if "_id" in session:
+        session.pop("_id")
+
+    cookie_name = current_app.config.get("REMEMBER_COOKIE_NAME")
+    if cookie_name in request.cookies:
+        session["_remember"] = "clear"
+    if "_remember_seconds" in session:
+        session.pop("_remember_seconds")
+
+    user_logged_out.send(current_app._get_current_object(), user=user)
+
+    current_app.login_manager._update_request_context_with_user()
+
     flash(notify_success("Successfully logged out"))
     return redirect(url_for("www.index"))
     # return redirect(url_for("auth.login"))
