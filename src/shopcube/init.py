14b7b8b6a8f51b3b65ce8d657c5da22a03d6effa
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-09-27 00:34:08 +0400

    chore: rm marshmallow

--- src/shopcube/init.py
+++ src/shopcube/init.py
@@ -5,7 +5,6 @@ import os
 
 from flask_login import LoginManager
 from flask_mailman import Mail
-#from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import DOCUMENTS
@@ -21,9 +20,7 @@ themes_path = os.path.join(static_path, "themes")  # don't remove
 installed_packages = []
 
 db = SQLAlchemy()
-# ma = Marshmallow()
-
-ma = None
+ 
 login_manager = LoginManager()
 migrate = Migrate()
 mail = Mail()
@@ -46,7 +43,6 @@ def configure_all_uploads(app):
 def load_extensions(app):
     migrate.init_app(app, db)
     db.init_app(app)
-    # ma.init_app(app)
     mail.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
