687ee6f3bbcc69d8fc381fbad9d04b722d8e12cf
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-09-27 00:08:19 +0400

    chore: fix init

--- src/shopcube/init.py
+++ src/shopcube/init.py
@@ -5,32 +5,7 @@ import os
 
 from flask_login import LoginManager
 from flask_mailman import Mail
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
-
-# from flask_marshmallow import Marshmallow, uncommented as not updated to support flask 2.x
-
-root_path = os.path.dirname(os.path.abspath(__file__))  # don't remove
-static_path = os.path.join(root_path, "static")  # don't remove
-modules_path = os.path.join(root_path, "modules")  # don't remove
-themes_path = os.path.join(static_path, "themes")  # don't remove
-installed_packages = []  # don't remove
-
-installed_packages = []
-
-db = SQLAlchemy()
-# ma = Marshmallow()
-login_manager = LoginManager()
-migrate = Migrate()
-mail = Mail()
-csrf = CSRFProtect()
-
-import os
-
-from flask_login import LoginManager
-from flask_mailman import Mail
-from flask_marshmallow import Marshmallow
+#from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import DOCUMENTS
@@ -43,13 +18,16 @@ root_path = os.path.dirname(os.path.abspath(__file__))  # don't remove
 static_path = os.path.join(root_path, "static")  # don't remove
 modules_path = os.path.join(root_path, "modules")  # don't remove
 themes_path = os.path.join(static_path, "themes")  # don't remove
+installed_packages = []
 
 db = SQLAlchemy()
-ma = Marshmallow()
+# ma = Marshmallow()
+
+ma = None
 login_manager = LoginManager()
 migrate = Migrate()
-csrf = CSRFProtect()
 mail = Mail()
+csrf = CSRFProtect()
 
 productphotos = UploadSet("productphotos", IMAGES)
 categoryphotos = UploadSet("categoryphotos", IMAGES)
