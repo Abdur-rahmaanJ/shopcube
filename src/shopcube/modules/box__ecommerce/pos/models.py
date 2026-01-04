5fc60ae41fae2fcf8beda528e558d1570019c473
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-01-04 10:04:42 +0400

    fix: POS data model by introducing TransactionItem join table

--- src/shopcube/modules/box__ecommerce/pos/models.py
+++ src/shopcube/modules/box__ecommerce/pos/models.py
@@ -10,12 +10,34 @@ class Transaction(db.Model):
 
     chashier_id = db.Column(db.Integer)
     time = db.Column(db.DateTime, default=datetime.now)
-    quantity = db.Column(db.Integer)
-    price = db.Column(db.Numeric(10, 2))
-    total_amount = db.Column(db.Numeric(10, 2))
+    total_amount = db.Column(db.Numeric(10, 2), default=0.0)
     method_of_payment = db.Column(db.String(50))
 
-    product = db.relationship('Product', backref='transaction', lazy=True)
+    items = db.relationship('TransactionItem', backref='transaction', lazy=True, cascade="all, delete-orphan")
+
+    def add(self):
+        db.session.add(self)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class TransactionItem(db.Model):
+    __tablename__ = "transaction_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), nullable=False)
+    product_barcode = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    unit_price = db.Column(db.Numeric(10, 2), nullable=False)
 
     def add(self):
         db.session.add(self)
