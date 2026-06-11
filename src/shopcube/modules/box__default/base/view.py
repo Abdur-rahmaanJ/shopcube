1a2c8c7a159b0662f2364a0c310682e3ef9d538f
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-06-12 02:45:09 +0400

    restore box__default/base (needed for module_base template)

---
+++ src/shopcube/modules/box__default/base/view.py
@@ -0,0 +1,32 @@
+from shopyo.api.module import ModuleHelp
+
+# from flask import render_template
+# from flask import url_for
+# from flask import redirect
+# from flask import flash
+# from flask import request
+
+# from shopyo.api.html import notify_success
+# from shopyo.api.forms import flash_errors
+
+mhelp = ModuleHelp(__file__, __name__)
+globals()[mhelp.blueprint_str] = mhelp.blueprint
+module_blueprint = globals()[mhelp.blueprint_str]
+
+
+@module_blueprint.route("/")
+def index():
+    return mhelp.info["display_string"]
+
+
+# If "dashboard": "/dashboard" is set in info.json
+#
+# @module_blueprint.route("/dashboard", methods=["GET"])
+# def dashboard():
+
+#     context = mhelp.context()
+
+#     context.update({
+
+#         })
+#     return mhelp.render('dashboard.html', **context)
